bc84dc0d4bc13fa5d4612f6a74b8d8cf20758b06
Daniel Lucas <alves.daniel4320@gmail.com>
2025-03-06 02:25:24 -0300

    Feat: Added function for the server to accept connections.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import time
 from threading import Thread
 
 class Packet:
@@ -20,10 +21,37 @@ class ClientConnection:
             self.client_socket.close();
 
     def run(self):
-        ...
+        while (self.isRunning):
+            try:
+                
+                data = self.client_socket.recv(1024);
+                
+                if not data:
+                    self.isRunning = False;
+                    self.disconnect();
+                    break;
+                
+                print(data);
+            except Exception as e:
+                print(f"Error: {e}");
 
 class Server:
-    ...
+    
+    def __init__(self, host="0.0.0.0", port=8080):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+        self.server_socket.bind((host, port));
+        self.server_socket.listen();
+        self.clients = [];
+        
+    def run(self):
+        print("Servidor iniciado. Aguardando conexões...");
+        while True:
+            client_socket, addr = self.server_socket.accept();
+            client = ClientConnection(client_socket, addr);
+            
+            self.clients.append(client);
+            
+            print(f'Cliente {addr} conectado.');
     
 if (__name__ == "__main__"):
     server = Server()
